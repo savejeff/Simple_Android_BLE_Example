a64cce4e0405525264c133dfa2671acd294962fe
Jan Dreiskemper <save_jeff@hotmail.de>
2020-06-08 12:07:47 +0200

    Some cleanups

--- example/ESP_BLE_Example/ESP_BLE_Example.ino
+++ example/ESP_BLE_Example/ESP_BLE_Example.ino
@@ -1,23 +1,17 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
+    
+Android Test Server
 
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+Creating 2 Services
+    1.) Test Service with one Characteristic for Read, Write and Notify
+        if a text like 'HalloWorld#' is written over Serial it will be set to the Test Characteristic.
+        Note that you need to end you Serial input with a Hash (#) Symbol
+        The current value of the Test Characteristic is periodically printed to Serial
 
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   In this example rxValue is the data received (only accessible inside that function).
-   And LoopCount_UART is the data to be sent, in this example just a byte incremented every second. 
+    2.) UART Service with RX and TX to read and write Text
+        if a device is connected, the UART Service will send periodically its loopcount as text to client.
+        if a client device writes to UART RX it will be printed to Serial
+   
 */
 #include <String.h>
 
